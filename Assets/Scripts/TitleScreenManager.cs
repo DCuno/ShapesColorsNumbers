2e79a16a9f9a029cacef5ebe330a176133b11a65
David Cuno <47536227+DCuno@users.noreply.github.com>
2020-11-09 20:19:38 -0500

    TitleScreen Start

---
+++ Assets/Scripts/TitleScreenManager.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TitleScreenManager : MonoBehaviour
+{
+    public void StartButton()
+    {
+        Debug.Log("Hello, Jello");
+    }
+}
