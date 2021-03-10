8e5a5dbd7ef720ff6374e5ebe12b4183733eec0f
David Cuno <cunodavida@gmail.com>
2021-03-10 16:58:03 -0500

    Audio Partially Implemented
    - Shapes, Colors, and Numbers audio implemented.
    - Audio Script attached to Audio Source and that now loads the audio files for reference from Polygon.
    * Still need to implement audio for each lesson.

--- Assets/Scripts/Spawner.cs
+++ Assets/Scripts/Spawner.cs
@@ -88,7 +88,7 @@ public class Spawner : MonoBehaviour
         return i switch
         {
             0 => new Color(0.784f, 0.216f, 0.216f), // Red
-            1 => new Color(1.0f, 0.831f, 0.0f), // Green
+            1 => new Color(0.0f, 0.831f, 0.0f), // Green
             2 => new Color(0f, 0.33f, 0.831f), // Blue
             3 => new Color(1.0f, 0.8f, 0.0f), // Yellow
             4 => new Color(1.0f, 0.64f, 0.0f),// Orange
@@ -102,7 +102,7 @@ public class Spawner : MonoBehaviour
         arr[Random.Range(0, arr.Count)] switch
         {
             Colors.Red => new Color(0.784f, 0.216f, 0.216f), // Red
-            Colors.Green => new Color(1.0f, 0.831f, 0.0f), // Green
+            Colors.Green => new Color(0.0f, 0.831f, 0.0f), // Green
             Colors.Blue => new Color(0f, 0.33f, 0.831f), // Blue
             Colors.Yellow => new Color(1.0f, 0.8f, 0.0f), // Yellow
             Colors.Orange => new Color(1.0f, 0.4f, 0.0f), // Orange
@@ -143,17 +143,17 @@ public class Spawner : MonoBehaviour
 
     private Spawner.Colors UnityColorToEnumColor(Color color)
     {
-        if (color == Color.red)
+        if (color == new Color(0.784f, 0.216f, 0.216f))
             return Colors.Red;
-        else if (color == Color.green)
+        else if (color == new Color(0.0f, 0.831f, 0.0f))
             return Colors.Green;
-        else if (color == Color.blue)
+        else if (color == new Color(0f, 0.33f, 0.831f))
             return Colors.Blue;
-        else if (color == Color.yellow)
+        else if (color == new Color(1.0f, 0.8f, 0.0f))
             return Colors.Yellow;
-        else if (color == new Color(1.0f, 0.64f, 0.0f)) // Orange
+        else if (color == new Color(1.0f, 0.4f, 0.0f))
             return Colors.Orange;
-        else if (color == new Color(0.5f, 0f, 0.5f)) // Purple
+        else if (color == new Color(0.443f, 0.216f, 0.784f))
             return Colors.Purple;
         else // White
             return Colors.White;
