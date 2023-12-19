5aebac5c28261fe1b7d9a8966a5628a74b7b44c7
David Cuno <cunodavida@gmail.com>
2023-12-18 19:00:01 -0500

    Options UI, Sound Options Fix
    - Updated Options UI to new style
    - Sound/Music is now saving properly

--- Assets/Scripts/TitleScreenManager.cs
+++ Assets/Scripts/TitleScreenManager.cs
@@ -8,17 +8,22 @@ using UnityEngine.UI;
 public class TitleScreenManager : MonoBehaviour
 {
     [SerializeField] private AudioMixer _mixer;
+    [SerializeField] private OptionsManager _optionsManager;
     private Audio _sfx;
 
     public GameObject[] UIButtonImages;
 
     void Start()
     {
+        //if (_optionsManager == null && _optionsManager.name == "DontDestroyOnLoad")
+            DontDestroyOnLoad(_optionsManager);
         // Setup Volumes from Player Prefs
         _mixer.SetFloat("Music", OptionsManager.SliderToDecibelMusic(PlayerPrefs.GetFloat("Music", 5f)));
         _mixer.SetFloat("SFX", OptionsManager.SliderToDecibelSFX(PlayerPrefs.GetFloat("SFX", 5f)));
-
         _sfx = GameObject.FindGameObjectWithTag("SFXSource").GetComponent<Audio>();
+        _mixer.GetFloat("Music", out float value);
+        _mixer.GetFloat("SFX", out float value2);
+        print("Music Vol: " + " " + value + " SFX Vol: " + value2);
     }
 
     private void Update()
