d78b4783504f3c6ad670d8dff46b57f5096da89f
David Cuno <cunodavida@gmail.com>
2024-01-04 00:44:40 -0500

    Back Button
    - Added hand for telegraphing how to use the back button
    ~ Adjusting function of back button to properly work

--- Assets/Scripts/GameBackButton.cs
+++ Assets/Scripts/GameBackButton.cs
@@ -5,6 +5,7 @@ using UnityEngine.SceneManagement;
 
 public class GameBackButton : MonoBehaviour
 {
+    public bool Touching = false;
     Vector2 _startPos;
     Spawner _spawner;
     Animator _anim;
@@ -18,15 +19,59 @@ public class GameBackButton : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        //CheckTouch();
+    }
+
+    private void CheckTouch()
+    {
+        if (Input.touchCount > 0)
+        {
+            // Loop through all active touches
+            for (int i = 0; i < Input.touchCount; i++)
+            {
+                Touch touch = Input.GetTouch(i);
+
+                if (touch.phase == TouchPhase.Began)
+                {
+                    // Convert touch position to world position
+                    Vector3 worldTouchPosition = Camera.main.ScreenToWorldPoint(touch.position);
+
+                    // Perform a raycast to detect if the object is touched
+                    RaycastHit2D hit = Physics2D.Raycast(worldTouchPosition, Vector2.zero);
+
+                    // Check if the object was hit by the raycast
+                    if (hit.collider != null && hit.collider.gameObject == gameObject)
+                    {
+                        //Pop();
+                    }
+                }
+            }
+        }
     }
 
     private void OnMouseDown()
     {
-        _anim.SetTrigger("Press");
+        SetButton();
     }
 
-    private void OnMouseDrag()
+    private void OnMouseUp()
+    {
+        ResetButton();
+    }
+
+    public void SetButton()
+    {
+        _anim.SetBool("Press 0", true);
+        Touching = true;
+    }
+
+    public void ResetButton()
+    {
+        _anim.SetBool("Press 0", false);
+        Touching = false;
+    }
+
+    /*private void OnMouseDrag()
     {
         //print("mouse: " + Camera.main.ScreenToWorldPoint(Input.mousePosition) + " transform: " + gameObject.transform.position/2f);
         if (SceneManager.GetActiveScene().name == "FunModeGameScene2")
@@ -40,5 +85,5 @@ public class GameBackButton : MonoBehaviour
             if (Camera.main.ScreenToWorldPoint(Input.mousePosition).x > gameObject.transform.position.x / 8f || Camera.main.ScreenToWorldPoint(Input.mousePosition).y < gameObject.transform.position.y / 2f)
                 _spawner.LeaveLessons();
         }
-    }
+    }*/
 }
